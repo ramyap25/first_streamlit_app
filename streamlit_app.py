264dc1d282a97fd240382385e519354e902d8cc2
Ramya_appdevuser <104056057+ramyap25@users.noreply.github.com>
2022-04-20 12:58:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 streamlit.title("My Mom\'s New Healthy Diner")
 streamlit.header("Breakfast Favorites")
 streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oat Meal')
-streamlit.text(':tropical_drink: Fruit Punch, Maggiee Noodles :ramen:')
+streamlit.text('(":tropical_drink:") Fruit Punch, Maggiee Noodles :ramen:')
 streamlit.text( ':chicken: Hard-Boiled Free-Ranged Egg')
 streamlit.text(':green_salad: Kale,Spinach & Rocket smoothie') 
 streamlit.text(' :avocado: :bread:  Avocado Toast')
+streamlit.text(~heart
